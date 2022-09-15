583e667081b7d6e0c83ed7d2094a76cf44cc3bba
Nikolay Spravtsev <nicksprv@icloud.com>
2022-09-16 00:02:30 +0300

    Made files for MVC

--- SettingAppMVC/Sources/Scenes/Controllers/SettingsController.swift
+++ SettingAppMVC/Sources/Scenes/Controllers/SettingsController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ SettingAppMVC/Sources/Scenes/Views/SettingsView.swift
@@ -0,0 +1,11 @@
+//
+//  SettingsView.swift
+//  SettingAppMVC
+//
+//  Created by Nick Spravtsev on 16.09.2022.
+//
+
+import UIKit
+
+class SettingsView: UIView {
+}
